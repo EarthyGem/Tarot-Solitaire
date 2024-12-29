648bfe68413e6e8ff7befaa0beadfd429349cd50
Errick Miron <highsman@lilaverse.app>
2024-12-28 21:29:17 -0800

    Initial Commit

---
+++ Tarot Solitaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tarot Solitaire
+//
+//  Created by Errick Williams on 12/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
